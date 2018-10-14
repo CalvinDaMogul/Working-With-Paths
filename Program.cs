a8f4690534d043a306642d54494cfa42b26c6838
Calvin J Foster <cal.j.foster@gmail.com>
2018-10-14 16:45:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld.sln";
+        }
+    }
+}
